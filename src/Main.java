a01e0dcd47b5500f9b195b4b268a776fa1a56157
Moisuk-Dmitri <moisuk.d@gmail.com>
2024-05-22 13:36:42 +0300

    corrections

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 0) {
+        } else {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
 
